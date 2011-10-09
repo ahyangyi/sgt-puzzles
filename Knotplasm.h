2e91ee0d8ae080665635c7f3c8cefacbf52a87a9
Yi Yang <ahyangyi@gmail.com>
2011-10-09 16:05:14 +0800

    Where the fun begins.

---
+++ Knotplasm.h
@@ -0,0 +1,31 @@
+#ifndef Knotplasm_HEADER
+#define Knotplasm_HEADER
+
+#include <KIcon>
+ 
+#include <Plasma/Applet>
+#include <Plasma/Svg>
+ 
+class QSizeF;
+ 
+// Define our plasma Applet
+class Knotplasm : public Plasma::Applet
+{
+    Q_OBJECT
+    public:
+        // Basic Create/Destroy
+        Knotplasm(QObject *parent, const QVariantList &args);
+        ~Knotplasm();
+ 
+        // The paintInterface procedure paints the applet to screen
+        void paintInterface(QPainter *p,
+                const QStyleOptionGraphicsItem *option,
+                const QRect& contentsRect);
+    void init();
+ 
+    private:
+        Plasma::Svg m_svg;
+        KIcon m_icon;
+};
+ 
+#endif
