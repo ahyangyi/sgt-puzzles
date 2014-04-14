d7549b5736f3791ca9ad7fdf24261c3f7439e68c
Yi Yang@WhiteImp <ahyangyi@gmail.com>
2014-04-14 15:28:33 +0800

    Created a theme matching for Unequal.

--- Kpgh-unequal.cpp
+++ Kpgh-unequal.cpp
@@ -1,4 +1,5 @@
 #include "Knotplasmagamehandler.h"
+#include <limits>
 
 UnequalGameHandler::UnequalGameHandler(const GameHandlerFactories& factories): DefaultGameHandler(factories)
 {
@@ -26,7 +27,7 @@ void UnequalGameHandler::preprocessBatch(QList<std::shared_ptr<KnotRendererBatch
     /*
      * Step 2: remove all polygons with size 4, which are frames of the squares, which will be better represented with plasma frames.
      */
-    for (auto it = batch.begin(); it != batch.end();)
+    for (auto it = batch.begin(); it != batch.end(); ++ it)
     {
         if (typeid(**it) == typeid(KnotRendererBatch::KnotBatchPolyAction))
         {
@@ -34,13 +35,38 @@ void UnequalGameHandler::preprocessBatch(QList<std::shared_ptr<KnotRendererBatch
             
             if (poly->polygon.size() == 4)
             {
-                it = batch.erase(it);
+                qreal x1 = std::numeric_limits<qreal>::max();
+                qreal y1 = std::numeric_limits<qreal>::max();
+                qreal x2 = std::numeric_limits<qreal>::min();
+                qreal y2 = std::numeric_limits<qreal>::min();
+
+                for (auto it = poly->polygon.begin(); it != poly->polygon.end(); ++ it)
+                {
+                    if (it->x() < x1) x1 = it->x();
+                    if (it->y() < y1) y1 = it->y();
+                    if (it->x() > x1) x2 = it->x();
+                    if (it->y() > y1) y2 = it->y();
+                }
+                auto neo = std::shared_ptr<KnotRendererPlasma::KnotBatchAction>(m_factories.rect_factory->getAction(
+                    x1, y1, x2 - x1, y2 - y1, true, true, true, true, KnotPlasmaRectAction::SUNKEN));
+                
+                *it = neo;
             }
-            else
-                ++ it;
         }
-        else
-            ++ it;
+    }
+
+    /*
+     * Step 3: remove all rectangles.
+     */
+
+    for (auto it = batch.begin(); it != batch.end();)
+    {
+        if (typeid(**it) == typeid(KnotRendererBatch::KnotBatchRectAction))
+        {
+            it = batch.erase(it);
+            continue;
+        }
+        ++ it;
     }
 }
 UnequalGameHandler::~UnequalGameHandler()
